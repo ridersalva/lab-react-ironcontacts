739921167ac75192cf3f1e294a64784f14afba8f
salva <saersalva@gmail.com>
2022-02-22 17:55:02 +0100

    pp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import contacts from './contacts.json';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [firstFive, setFirstfive] = useState(contacts.slice(0, 5)) 
+
+  console.log(firstFive)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th> Picture</th>
+            <th> Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {firstFive.map((elm, idx) => {
+            return (
+              <tr key={idx}>
+                <td><img src={elm.pictureUrl} alt={elm.name} /> </td>
+                <td> <p>{elm.name}</p></td>
+                <td><p>{elm.popularity}</p></td>
+                <td> {elm.wonOscar ? <p>🏆</p> : <p> Good Luck!</p>}</td>
+                <td> {elm.wonEmmy ? <p>🏆</p> : <p> Good Luck Friend!</p>}</td>
+              </tr>
+            )
+          }
+          )}
+        </tbody>
+      </table>
+      {/* <button onClick={ addContacts} > Add Contact</button> */}
     </div>
   );
 }
